174e8511679431ecbea30045a570b351df56e5ac
jhinzmann <jhinzmann.gt@googlemail.com>
2015-06-08 01:14:50 +0200

    replaced tabs with spaces
    replaced tabs with spaces for consistent whitespace in js files.

--- exampleUsage.js
+++ exampleUsage.js
@@ -1,6 +1,6 @@
 (function(){
-	"use strict";
-	var dataSet = [{
+  "use strict";
+  var dataSet = [{
       something: 20
     },{
       something: 30
@@ -10,15 +10,15 @@
       something: 80
     }];
 
-	var dataSet2 = [{something: 40},{something:90}];
+  var dataSet2 = [{something: 40},{something:90}];
 
-	var myChart = d3.myReusableResponsiveChart()
-	    .value(function(d) { return d.something })
-	    .height(400);
+  var myChart = d3.myReusableResponsiveChart()
+      .value(function(d) { return d.something })
+      .height(400);
 
-	var chart = d3.select('#chartContainer').selectAll('div')
-	    .data([dataSet, dataSet2])
-	    .enter()
-	  .append('div')
-	    .call(myChart);
+  var chart = d3.select('#chartContainer').selectAll('div')
+      .data([dataSet, dataSet2])
+      .enter()
+    .append('div')
+      .call(myChart);
 })();
